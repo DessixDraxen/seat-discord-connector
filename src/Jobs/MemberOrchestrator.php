2e1b2b716d59e429c23c57318f56149f7ecad458
DessixDraxen <46351657+DessixDraxen@users.noreply.github.com>
2019-01-07 19:18:53 -0500

    Fixed bug in Nickname enforcement
    Fixed bug in MemberOrchestrator.php to enforce a Discord user name to match to the Seat Main Character name on the role enforcement pass. This is to prevent a discord user from changing their nickname after the initial invite sets their nickname (in the assumption the change nickname discord permission is granted to the user). This is also to automatically update the discord nickname if the SEAT user changes their main character.
    
    The original version would attempt to do this on the role enforcement pass but would only try to do it if a role had changed and then in that case, fail to enforce the nickname as the $options array was never assigned back to by the array_merge() call.

--- src/Jobs/MemberOrchestrator.php
+++ src/Jobs/MemberOrchestrator.php
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+ 
 namespace Warlof\Seat\Connector\Discord\Jobs;
 
 use Illuminate\Support\Facades\Redis;
@@ -36,22 +36,22 @@ class MemberOrchestrator extends DiscordJobBase
      * @var array
      */
     protected $tags = ['orchestrator'];
-
+	
     /**
      * @var GuildMember
      */
     private $member;
-
+	
     /**
      * @var bool
      */
     private $terminator;
-
+	
     /**
      * @var int
      */
     public $tries = 100;
-
+	
     /**
      * ConversationOrchestrator constructor.
      * @param string $member
@@ -60,17 +60,17 @@ class MemberOrchestrator extends DiscordJobBase
     public function __construct(GuildMember $member, bool $terminator = false)
     {
         logger()->debug('Initialising member orchestrator for ' . $member->nick);
-
+		
         $this->terminator = $terminator;
         $this->member = $member;
-
+		
         array_push($this->tags, 'member_id:' . $member->user->id);
-
+		
         // if the terminator flag has been passed, append terminator into tags
         if ($this->terminator)
             array_push($this->tags, 'terminator');
     }
-
+	
     /**
      * @throws DiscordSettingException
      * @throws \Seat\Services\Exceptions\SettingException
@@ -79,40 +79,40 @@ class MemberOrchestrator extends DiscordJobBase
     {
         if (is_null(setting('warlof.discord-connector.credentials.bot_token', true)))
             throw new DiscordSettingException();
-
+			
         if (is_null(setting('warlof.discord-connector.credentials.guild_id', true)))
             throw new DiscordSettingException();
-
+			
         Redis::throttle('seat-discord-connector:jobs.member_orchestrator')->allow(20)->every(10)->then(function() {
-
+		
             // in case terminator flag has not been specified, proceed using user defined mapping
             if (! $this->terminator) {
                 $this->processMappingBase();
                 return;
             }
-
+			
             $this->updateMemberRoles([]);
-
+			
         }, function() {
-
+		
             logger()->warning('A MemberOrchestrator job is already running. Delay job by 10 seconds.');
-
+			
             $this->release(10);
-
+			
         });
     }
-
+	
     /**
      * Set terminator flag to true.
      */
     public function setTerminatorFlag()
     {
         $this->terminator = true;
-
+		
         if (! in_array('terminator', $this->tags))
             array_push($this->tags, 'terminator');
     }
-
+	
     /**
      * Prepare roles mapping and update user if required.
      *
@@ -120,33 +120,34 @@ class MemberOrchestrator extends DiscordJobBase
      */
     private function processMappingBase()
     {
+        $roles = [];
         $nickname = null;
         $pending_drops = collect();
         $pending_adds = collect();
-
+		
         $discord_user = DiscordUser::where('discord_id', $this->member->user->id)->first();
-
+		
         if (! is_null($discord_user)) {
             if (! is_null($discord_user->group->main_character))
                 $nickname = $discord_user->group->main_character->name;
-
+				
             foreach ($this->member->roles as $role_id) {
                 if (! Helper::isAllowedRole($role_id, $discord_user))
                     $pending_drops->push($role_id);
             }
-
+			
             $roles = Helper::allowedRoles($discord_user);
-
+			
             foreach ($roles as $role_id) {
                 if (! in_array($role_id, $this->member->roles))
                     $pending_adds->push($role_id);
             }
-
-            if ($pending_adds->count() > 0 || $pending_drops->count() > 0)
-                $this->updateMemberRoles($roles, $nickname);
+			
+			if ($pending_adds->count() > 0 || $pending_drops->count() > 0 || ($this->member->nick != $nickname && ! is_null($nickname))
+				$this->updateMemberRoles($roles, $nickname);
         }
     }
-
+	
     /**
      * Update Discord user with new role mapping and nickname if required
      *
@@ -161,12 +162,12 @@ class MemberOrchestrator extends DiscordJobBase
             'user.id'  => $this->member->user->id,
             'roles'    => $roles,
         ];
-
-        if ($this->member->nick != $nickname && ! is_null($nickname))
-            array_merge($options, [
+		
+        if (! is_null($nickname))
+            $options = array_merge($options, [
                 'nick' => $nickname
             ]);
-
+			
         app('discord')->guild->modifyGuildMember($options);
     }
 }
